e2071b6faea9765fbbab7b23f59d5bb04042e93a
117503445 <t117503445@gmail.com>
2022-04-21 10:07:30 +0800

    feat: update put.py

--- client/put.py
+++ client/put.py
@@ -3,12 +3,28 @@ Author: HaoTian Qi
 Date: 2022-02-21 17:34:53
 Description: 
 LastEditors: HaoTian Qi
-LastEditTime: 2022-03-15 16:03:38
+LastEditTime: 2022-03-28 18:49:41
 '''
 import requests
 from datetime import datetime
 
 
+a = 0
 
-for i in range(1):
-    requests.put('http://127.0.0.1:12380/my-key',data=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+def req(n):
+    global a
+    for _ in range(n):
+        a += 1
+        print(a)
+        requests.put(f'http://127.0.0.1:12380/my-key', data=f'{a}')
+
+
+req(16)
+
+# 4 add config entry
+# 16 normal entry
+
+
+input()
+req(15)
